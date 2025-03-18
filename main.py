51ba98b6ee3dc5a96737d39136fe6b3a4db7941c
gkn2 <112503427+gkn2@users.noreply.github.com>
2025-03-18 11:36:50 -0400

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,4 @@
+counter: number = 1
+while counter < 11:
+    game.splash(counter)
+    counter -= 2
---
+++ main.ts
@@ -0,0 +1,5 @@
+let counter = 1
+while (counter < 11) {
+    game.splash(counter)
+    counter -= 2
+}
